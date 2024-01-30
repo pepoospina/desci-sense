de2696ca3b29fbf66958e4be02ee2a1c08baea48
pepo <pepo.ospina@gmail.com>
2024-01-30 12:12:45 +0100

    added prettier with import order

--- app/webapp/src/App.tsx
+++ app/webapp/src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
+import { AppContainer } from './app/AppContainer';
+import { GlobalStyles } from './app/GlobalStyles';
+import { ResponsiveApp } from './app/ResponsiveApp';
+import { ThemedApp } from './app/ThemedApp';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <GlobalStyles />
+      <ThemedApp>
+        <ResponsiveApp>
+          <BrowserRouter>
+            <AppContainer></AppContainer>
+          </BrowserRouter>
+        </ResponsiveApp>
+      </ThemedApp>
     </div>
   );
 }
