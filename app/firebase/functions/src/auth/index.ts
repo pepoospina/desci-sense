fea13a9ec0c774fb084856a6634bce0fc10b7df1
pepo <pepo.ospina@gmail.com>
2024-01-31 10:21:14 +0100

    removed eslint

--- app/firebase/functions/src/auth/index.ts
+++ app/firebase/functions/src/auth/index.ts
@@ -2,7 +2,7 @@ import express from 'express';
 import * as functions from 'firebase-functions';
 
 import { RUNTIME_OPTIONS } from '../config/RUNTIME_OPTIONS';
-import { app } from   '../instances/app';
+import { app } from '../instances/app';
 import { authCodeController } from './controllers/code.controller';
 
 const authRouter = express.Router();
