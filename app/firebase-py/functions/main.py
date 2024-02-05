88fab574f493a49ed41dfe3a35e4e4a6edde4a41
pepo <pepo.ospina@gmail.com>
2024-02-05 12:41:46 +0100

    use pipenv

--- app/firebase-py/functions/main.py
+++ app/firebase-py/functions/main.py
@@ -4,16 +4,10 @@ from firebase_functions import https_fn
 # The Firebase Admin SDK to access Cloud Firestore.
 from firebase_admin import initialize_app
 
+from models.tagger import SM_FUNCTION_post_tagger
+
 app = initialize_app()
 
 @https_fn.on_request()
-def addmessage(req: https_fn.Request) -> https_fn.Response:
-    """Take the text parameter passed to this HTTP endpoint and insert it into
-    a new document in the messages collection."""
-    # Grab the text parameter.
-    original = req.args.get("text")
-    if original is None:
-        return https_fn.Response("No text parameter provided", status=400)
-
-    # Send back a message that we've successfully written the message
-    return https_fn.Response(f"Text {original} received!.")
+def post_tagger(request):
+    return SM_FUNCTION_post_tagger(request)
